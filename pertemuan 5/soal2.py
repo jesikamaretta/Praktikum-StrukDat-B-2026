2eee9d14caf09e36f7306cb39152fbbab87df694
jesikamaretta <jesika.maretta2660stydent.unri.ac.id>
2026-02-26 19:16:59 +0700

    Menghapus folder pertemuan 2 dan pertemuan 5

--- pertemuan 5/soal2.py
+++
@@ -1,8 +0,0 @@
-kumpulan_nilai = [("Andi", 85), ("Budi", 60), ("Cici", 90), ("Deni", 72)] 
-#Gunakan perulangan untuk memproses setiap tuple tersebut. Jika nilai >= 75, tampilkan: "Selamat [Nama], Anda Lulus!". Jika di bawah 75, tampilkan: "Maaf us remidi." 
-
-for i in kumpulan_nilai:
-    if i[1] >= 75:
-        print(f"Selamat{i[0]}, Anda Lulus")
-    else:
-        print(f"Maaf{i(0)}, Anda Tidak Lulus")
